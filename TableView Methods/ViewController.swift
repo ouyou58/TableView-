3d945b1c9e7a18fbca583743bb29e7c1e9616d10
ouyou58 <ouyou58@gmail.com>
2019-12-10 14:15:15 +0900

    Initial Commit

---
+++ TableView Methods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView Methods
+//
+//  Created by ouyou on 2019/12/10.
+//  Copyright © 2019 ouyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
